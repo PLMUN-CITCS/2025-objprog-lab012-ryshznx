2443fcdb1b94a3558df77e95b82fb0a7e9c1bc26
izzynx <miranokrizzypearl@plmun.edu.ph>
2025-02-19 13:40:58 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,13 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        greet();
+        printMessage("This is my message!");
+        }
+        public static void printMessage(String message) {
+            System.out.println(message);
+        }
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+
+    }
+}
